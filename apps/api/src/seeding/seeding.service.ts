51881c52f7bbeed467525c975022a60a344b16b7
Alee24 <mettoalex@gmail.com>
2026-04-30 21:23:38 +0300

    fix: correct field name phoneNumber -> mobile in seeding

--- apps/api/src/seeding/seeding.service.ts
+++ apps/api/src/seeding/seeding.service.ts
@@ -198,7 +198,7 @@ export class SeedingService {
         status: true,
         fname: 'Alex',
         lname: 'Metto',
-        phoneNumber: '0724454757',
+        mobile: '0724454757',
         emailVerifiedAt: new Date(),
       });
       await this.userRepo.save(adminUser);
