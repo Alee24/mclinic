6e22db0b53ea7bb64c15110a98eac1b71a6243c0
Alee24 <mettoalex@gmail.com>
2026-08-01 12:00:51 +0300

    fix(api): add NotificationModule to UsersModule

--- apps/api/src/users/users.module.ts
+++ apps/api/src/users/users.module.ts
@@ -4,9 +4,9 @@ import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { UserSubscriber } from './subscribers/user.subscriber';
 import { UsersController } from './users.controller';
-
+import { NotificationModule } from '../notification/notification.module';
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), NotificationModule],
   providers: [UsersService, UserSubscriber],
   controllers: [UsersController],
   exports: [UsersService],
