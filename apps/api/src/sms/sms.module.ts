407c02f772875296b3434d7713d111139fb313d6
Alee24 <mettoalex@gmail.com>
2026-08-03 20:30:12 +0300

    fix(sms): remove circular dependency - UsersModule/DoctorsModule not needed in SmsModule

--- apps/api/src/sms/sms.module.ts
+++ apps/api/src/sms/sms.module.ts
@@ -4,16 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SmsService } from './sms.service';
 import { SystemSetting } from '../system-settings/entities/system-setting.entity';
 import { SmsController } from './sms.controller';
-import { UsersModule } from '../users/users.module';
-import { DoctorsModule } from '../doctors/doctors.module';
 import { CommunicationLog } from '../notification/entities/communication-log.entity';
 
 @Module({
     imports: [
         HttpModule,
         TypeOrmModule.forFeature([SystemSetting, CommunicationLog]),
-        UsersModule,
-        DoctorsModule
     ],
     providers: [SmsService],
     exports: [SmsService],
